5da9899e2b5f4063e8c81ae4a99b387ee74c4eb7
JerryWang <jerry002@126.com>
2015-04-12 21:26:25 +0800

    chap1

--- src/main/java/com/jerry/crawler/chap1/RetrivePage.java
+++ src/main/java/com/jerry/crawler/chap1/RetrivePage.java
@@ -1,5 +1,6 @@
 package com.jerry.crawler.chap1;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +11,10 @@ import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.htmlparser.Parser;
+import org.htmlparser.Tag;
+import org.htmlparser.util.ParserException;
+import org.htmlparser.visitors.NodeVisitor;
 
 public class RetrivePage {
 	
@@ -20,7 +25,7 @@ public class RetrivePage {
 //		httpClient.getHostConfiguration().setProxy("192.168.1.1", 8000);
 	}
 	
-	public static boolean downloadPage(String path) throws HttpException, IOException {
+	public static boolean downloadPage(String path) throws HttpException, IOException, ParserException {
 		InputStream input = null;
 		OutputStream output = null;
 		
@@ -41,12 +46,17 @@ public class RetrivePage {
 		// 针对状态码进行处理
 		if(statusCode == HttpStatus.SC_OK) {
 			input = post.getResponseBodyAsStream();
-			
+			String charset = post.getResponseCharSet();
 			// 得到文件名	
 			String filename = path.substring(path.lastIndexOf('/') + 1);
-			
+			filename += System.currentTimeMillis();
+				
+			File tempFile = new File(filename);
+			if(!tempFile.exists()) {
+				tempFile.createNewFile();
+			}
 			// 获取文件输出流
-			output = new FileOutputStream(filename);
+			output = new FileOutputStream(tempFile);
 			
 			// 输出到文件
 			int tempByte = -1;
@@ -61,6 +71,55 @@ public class RetrivePage {
 			if(output != null)
 				output.close();
 			
+			// 使用 htmlpaser解析
+			Parser parser = new Parser(filename);
+			parser.setEncoding(charset);
+			NodeVisitor nodeVisitor = new NodeVisitor() {
+				private boolean flag = false;
+				private int index = -1;
+				
+				public void visitTag (Tag tag) {
+					if(tag.getTagName().equals("TBODY")) {
+						System.out.println("begin...");
+						flag = true;
+						index = 0;
+					}
+					
+					if(tag.getTagName().equals("TD") && flag) {
+						switch (index) {
+						case 0:
+							System.out.println("from_to :" + tag.toPlainTextString().trim());
+							break;
+						case 1:
+							System.out.println("carrier :" + tag.toPlainTextString().trim());
+							break;
+						case 2:
+							System.out.println("type :" + tag.toPlainTextString().trim());
+							break;
+						case 3:
+							System.out.println("number :" + tag.toPlainTextString().trim());
+							break;
+						case 4:
+							System.out.println("dicount :" + tag.toPlainTextString().trim());
+							break;
+						case 5:
+							System.out.println("price :" + tag.toPlainTextString().trim());
+							break;
+						default:
+							break;
+						}
+						index ++;
+					}
+				}
+				
+				public void visitEndTag (Tag tag) {
+					if(tag.getTagName().equals("TBOOY")) {
+						System.out.println("end...");
+						flag = false;
+					}
+				}
+			};
+			parser.visitAllNodesWith(nodeVisitor);
 			return true;
 		
 		}
