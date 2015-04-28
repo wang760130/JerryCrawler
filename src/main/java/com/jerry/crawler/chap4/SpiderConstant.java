87bf032dc4d65742a2a54deaa840cd3f99f54106
JerryWang <jerry002@126.com>
2015-04-28 22:21:38 +0800

    chap4

---
+++ src/main/java/com/jerry/crawler/chap4/SpiderConstant.java
@@ -0,0 +1,10 @@
+package com.jerry.crawler.chap4;
+
+public class SpiderConstant {
+	public final static int CHANNELLINKTYPE = 1;
+	
+	public final static int COMMONLINKTYPE = 2;
+	
+	public final static int OUTDOMAINLINKTYPE = 3;
+	
+}
