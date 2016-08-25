e55679495af7b443c9613fcd8293d8691c4a5aa8
JerryWang <jerry002@126.com>
2016-08-26 07:06:00 +0800

    重构一下下

--- src/main/java/com/jerry/crawler/example/chap3/IPTest.java
+++ src/main/java/com/jerry/crawler/example/chap3/IPTest.java
@@ -4,9 +4,9 @@ public class IPTest {
 
 	public static void main(String[] args) {
 		// 指定纯真数据库的文件名，所在文件夹
-//		IPSeeker ip = new IPSeeker("QQWry.Dat", "F:\\Git Repository\\JerryCrawler\\src\\main\\java\\com\\jerry\\crawler\\chap3\\QQWry.Dat");
+		IPSeeker ip = new IPSeeker("QQWry.Dat", "F:\\Git Repository\\JerryCrawler\\src\\main\\java\\com\\jerry\\crawler\\chap3\\QQWry.Dat");
 		// 测试IP 58.20.43.13
-//		System.out.println(ip.getIPLocation("58.20.43.13").getCountry() + ":"
-//				+ ip.getIPLocation("58.20.43.13").getArea());
+		System.out.println(ip.getIPLocation("58.20.43.13").getCountry() + ":"
+				+ ip.getIPLocation("58.20.43.13").getArea());
 	}
 }
